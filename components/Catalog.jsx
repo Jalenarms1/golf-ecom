11964a0a558fb45d242d9f61fcb21f4a0bb99a32
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2024-06-07 14:34:05 -0700

    touched up mobile styling, added func for sliding nav in mobile

--- components/Catalog.jsx
+++ components/Catalog.jsx
@@ -8,13 +8,15 @@ const Catalog = () => {
         <section id="catalog" className="py-12">
             <div className=" px-10 mx-auto">
                 <h2 className="text-3xl font-bold text-center mb-8">Shop Our Collection</h2>
-                <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-4 gap-8 ">
+                <div className="grid grid-cols-2  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
                 {products.reverse().slice(0,4).map(product => (
                     <div key={product.id} className="border p-4 mx-auto w-fit rounded-lg shadow-md shadow-zinc-200 hover:shadow-zinc-400 cursor-pointer flex flex-col justify-between">
-                        <Image src={product.image} alt={product.name} width={250} height={250} className="object-cover mb-4 rounded"/>
-                        <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
-                        <p className="text-gray-600 mb-4">{product.price}</p>
-                        <a href="#" className="bg-green-500 text-white px-4 py-2 rounded text-center">View Product</a>
+                        <div className=" h-[200px] mb-4">
+                            <Image src={product.image} alt={product.name} width={250} height={250} className="object-contain w-full h-full rounded"/>
+                        </div>
+                        <h3 className="sm:text-xl text-sm font-semibold mb-2">{product.name}</h3>
+                        <p className="text-gray-600 mb-4 sm:text-md text-sm">{product.price}</p>
+                        <a href="#" className="bg-green-500 text-white px-4 py-2 rounded text-center sm:text-md text-sm">View Product</a>
                     </div>
                 ))}
                 </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,11 +7,17 @@ import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { NavItem } from './NavItem';
 import { Menu } from 'lucide-react';
+import { ListCollapse } from 'lucide-react';
 
 const Navbar = () => {
   const pathname = usePathname()
 
   const[currPath, setCurrPath] = useState('Home')
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen)
+  }
 
   useEffect(() => {
     setCurrPath(pathname)
@@ -24,14 +30,14 @@ const Navbar = () => {
       <div className="px-2  flex justify-between items-center">
         <div className="flex items-center gap-4">
           <div className="flex items-center">
-            <LandPlot className="text-green-600 text-3xl mr-2" />
-            <span className="text-green-600 text-2xl font-bold">Golf Gear</span>
+            <LandPlot className="text-green-600 text-4xl mr-2" />
+            <span className="text-green-600 text-3xl font-bold merri-font">Golf Gear</span>
 
           </div>
           
         </div>
         <div className="flex sm:hidden">
-          <Menu />
+          <Menu onClick={toggleSidebar} className='text-white cursor-pointer active:text-zinc-400' />
         </div>
         <div className=" gap-4 items-center hidden sm:flex bg-black rounded-md p-2">
           <NavItem text={'Home'} path={currPath} active={currPath == '/'} />
@@ -52,6 +58,11 @@ const Navbar = () => {
           <SquareUser className='text-white cursor-pointer text-2xl' />
           <div className='border border-white'></div>
           <ShoppingCart className='text-white cursor-pointer text-2xl' />
+        </div>
+      </div>
+      <div id='sliding-nav' className={`bg-zinc-300 p-3 shadow-md shadow-zinc-900 border-l border-zinc-900 absolute right-0 z-[1] ${sidebarOpen ? 'active' : ''}`}>
+        <div className='flex '>
+        <ListCollapse onClick={toggleSidebar} className='text-black active:text-zinc-600 cursor-pointer' />
         </div>
       </div>
     </nav>
